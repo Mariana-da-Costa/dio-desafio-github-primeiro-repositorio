d7411e7719dbaf650a09870a48a506d643a32415
Mariana da Costa <marianacosta.engc@gmail.com>
2022-07-02 16:54:17 -0300

    Exercício de array

--- Modulo II/api-collections/app/src/main/java/com/example/api_collections/TesteArrayString.kt
+++ Modulo II/api-collections/app/src/main/java/com/example/api_collections/TesteArrayString.kt
@@ -1,7 +1,7 @@
 package com.example.api_collections
 
 fun main() {
-    val nomes = Array(3) {""}
+    val nomes = Array(3) { "" }
     nomes[0] = "Maria"
     nomes[1] = "Mariana"
     nomes[2] = "Ana"
@@ -13,4 +13,18 @@ fun main() {
     nomes.sort()
     nomes.forEach { println(it) }
 
+    nomes[0] = "Ana"
+    nomes[1] = "Maria"
+    nomes[2] = "Mariana"
+
+    println("----------------------------")
+    println("----------------------------")
+
+    nomes[2] = "Mari"
+
+    for (nome in nomes) {
+        println(nome)
+    }
+
+    println("Tamanho do array = " + nomes.size)
 }
