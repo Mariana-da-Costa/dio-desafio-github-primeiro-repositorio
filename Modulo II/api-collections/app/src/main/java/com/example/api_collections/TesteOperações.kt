5585c156963062208d1f950a95ff3ed2b6618d74
Mariana da Costa <marianacosta.engc@gmail.com>
2022-07-05 22:53:30 -0300

    Teste com outras operações

--- Modulo II/api-collections/app/src/main/java/com/example/api_collections/TesteOperações.kt
+++ Modulo II/api-collections/app/src/main/java/com/example/api_collections/TesteOperações.kt
@@ -8,11 +8,23 @@ fun main() {
     }
     println("-------------------------------")
     println("Maior salário: ${salarios.max()}")
-    println("Menos salário: ${salarios.min()}")
+    println("Menor salário: ${salarios.min()}")
     println("Media salarial: ${salarios.average()}")
 
     val salarioMaior2500 = salarios.filter { it > 2500.0 }
+
     println("-------------------------------")
     salarioMaior2500.forEach { println(it) }
+
+    println("-------------------------------")
+    println(salarios.count { it in 2000.0..5000.0 })
+
+    println("-------------------------------")
+    println(salarios.find { it == 2000.0 })
+    println(salarios.find { it == 500.0 })
+
+    println("-------------------------------")
+    println(salarios.any { it == 2000.0 })
+    println(salarios.any { it == 500.0 })
 
 }
