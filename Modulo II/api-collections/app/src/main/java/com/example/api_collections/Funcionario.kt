e2223579c91c4026da562e470490c1664fc2a3de
Mariana da Costa <marianacosta.engc@gmail.com>
2022-07-06 23:35:52 -0300

    Exercício com listas e operações

---
+++ Modulo II/api-collections/app/src/main/java/com/example/api_collections/Funcionario.kt
@@ -0,0 +1,13 @@
+package com.example.api_collections
+
+class Funcionario(
+    val nome: String,
+    val salario: Double,
+    val tipoContratacao: String
+) {
+    override fun toString(): String =
+        """
+            Nome:    $nome
+            Salário: $salario
+        """.trimIndent()
+}
