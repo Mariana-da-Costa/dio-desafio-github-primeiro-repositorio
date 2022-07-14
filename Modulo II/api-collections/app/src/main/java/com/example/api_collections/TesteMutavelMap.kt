aea254eb72aa3cd511621ac873e64d2675db61b3
Mariana da Costa <marianacosta.engc@gmail.com>
2022-07-13 23:31:38 -0300

    Exercícios com map

--- Modulo II/api-collections/app/src/main/java/com/example/api_collections/TesteMutavelMap.kt
+++ Modulo II/api-collections/app/src/main/java/com/example/api_collections/TesteMutavelMap.kt
@@ -7,6 +7,9 @@ fun main() {
 
     val repositorio = Repositorio<Funcionario>()
 
+    val pandora = Funcionario("Pandora", 5000.0, "PJ")
+    val princesa = Funcionario("Princesa", 5000.0, "PJ")
+
     repositorio.create(joao.nome, joao)
     repositorio.create(pedro.nome, pedro)
     repositorio.create(maria.nome, maria)
@@ -19,4 +22,23 @@ fun main() {
     println("----------------------")
     repositorio.remove(maria.nome)
     repositorio.findAll().forEach { println(it) }
+    
+    println("---------Adicionando funcionários-------------")
+
+    repositorio.create(pandora.nome, pandora)
+    repositorio.create(princesa.nome, princesa)
+
+    repositorio.findAll().forEach { println(it) }
+
+    val listaFuncionario = listOf(
+        Funcionario("Rafael", 9000.0, "CLT"),
+        Funcionario("Pandora", 7000.0, "PJ"),
+        Funcionario("Princesa", 7000.0, "CLT")
+    )
+
+    val repositorio2 = Repositorio<List<Funcionario>>()
+    repositorio2.create(listaFuncionario[0].nome, listaFuncionario)
+
+    println("-----Adicionando lista de funcionário no Map-----")
+    repositorio2.findAll().forEach { println(it) }
 }
